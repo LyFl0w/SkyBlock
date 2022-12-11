d2eb7d3526f3037f4183e569a944ff27329fffbd
LyFlow <off.lyflow@gmail.com>
2022-12-11 11:41:57 +0100

    - update CreateIslandEvent with PlayerIslandRequest

--- src/main/java/net/lyflow/skyblock/event/island/CreateIslandEvent.java
+++ src/main/java/net/lyflow/skyblock/event/island/CreateIslandEvent.java
@@ -2,14 +2,12 @@ package net.lyflow.skyblock.event.island;
 
 import net.lyflow.skyblock.SkyBlock;
 import net.lyflow.skyblock.island.IslandDifficulty;
-import net.lyflow.skyblock.island.MateStatus;
+import net.lyflow.skyblock.island.PlayerIslandRequest;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CreateIslandEvent extends Event implements Cancellable {
@@ -20,44 +18,19 @@ public class CreateIslandEvent extends Event implements Cancellable {
 
     public CreateIslandEvent(SkyBlock skyBlock, Player player, IslandDifficulty islandDifficulty) {
         try {
-            final Connection connection = skyBlock.getDatabase().getConnection();
+            final PlayerIslandRequest playerIslandRequest = new PlayerIslandRequest(skyBlock.getDatabase(), false);
 
-            // Check if player has an island
-            final PreparedStatement preparedStatement = connection.prepareStatement("""
-                    SELECT COUNT(*) FROM Island_Mate
-                    JOIN Player ON Player.id = Island_Mate.player_id
-                    WHERE Player.UUID = ?
-                    """);
-
-            preparedStatement.setString(1, player.getUniqueId().toString());
-
-            final boolean playerHasIsland = (0 != preparedStatement.executeQuery().getInt(1));
-
-            if(playerHasIsland) {
+            if(playerIslandRequest.playerHasIsland(player)) {
                 player.sendMessage("Tu ne peux pas avoir plusieurs îles en même temps !");
-                connection.close();
-
                 setCancelled(true);
                 return;
             }
 
-            final PreparedStatement preparedStatement2 = connection.prepareStatement("INSERT INTO Island (id_difficulty) VALUES (?)");
-            preparedStatement2.setInt(1, islandDifficulty.getDifficulty());
-            preparedStatement2.executeUpdate();
+            playerIslandRequest.createIsland(player, islandDifficulty);
 
-            final int primaryKey = preparedStatement2.getGeneratedKeys().getInt(1);
+            skyBlock.getDatabase().closeConnection();
 
-            final PreparedStatement preparedStatementPlayerID = connection.prepareStatement("SELECT id FROM Player WHERE UUID = ?");
-            preparedStatementPlayerID.setString(1, player.getUniqueId().toString());
-
-            final PreparedStatement preparedStatement3 = connection.prepareStatement("INSERT INTO Island_Mate VALUES (?, ?, ?)");
-            preparedStatement3.setInt(1, primaryKey);
-            preparedStatement3.setInt(2, preparedStatementPlayerID.executeQuery().getInt(1));
-            preparedStatement3.setInt(3, MateStatus.OWNER.getID());
-
-            preparedStatement3.execute();
-
-            connection.close();
+            player.sendMessage("§bCréation de votre île en cours §6§o(difficulté : "+islandDifficulty.name()+")");
         } catch(SQLException e) {
             throw new RuntimeException("Erreur lors de la récupération de la base lors de la création d'une île", e);
         }
