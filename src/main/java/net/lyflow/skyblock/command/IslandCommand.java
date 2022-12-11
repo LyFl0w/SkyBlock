6875df04188cc4cd6ea9dbb8b352f14e3c168785
LyFlow <off.lyflow@gmail.com>
2022-12-11 11:42:36 +0100

    - add functionality of command /island list

--- src/main/java/net/lyflow/skyblock/command/IslandCommand.java
+++ src/main/java/net/lyflow/skyblock/command/IslandCommand.java
@@ -2,6 +2,9 @@ package net.lyflow.skyblock.command;
 
 import net.lyflow.skyblock.SkyBlock;
 import net.lyflow.skyblock.island.IslandDifficulty;
+import net.lyflow.skyblock.island.IslandMate;
+import net.lyflow.skyblock.island.MateStatus;
+import net.lyflow.skyblock.island.PlayerIslandRequest;
 import net.lyflow.skyblock.utils.CommandUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,11 +13,13 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
+import java.sql.SQLException;
+import java.util.Comparator;
 import java.util.List;
 
 public class IslandCommand implements CommandExecutor, TabCompleter {
 
-    private final static String[] argsCompletion = new String[]{"create", "delete", "config", "invite", "kick", "setowner"};
+    private final static String[] argsCompletion = new String[]{"create", "delete", "config", "list", "invite", "kick", "setowner"};
 
     private final SkyBlock skyBlock;
     public IslandCommand(SkyBlock skyBlock) {
@@ -28,20 +33,51 @@ public class IslandCommand implements CommandExecutor, TabCompleter {
             final String sub = args[0];
             if (args.length == 1) {
                 // CREATE
-                if (sub.equalsIgnoreCase(argsCompletion[0])) {
+                if (sub.equalsIgnoreCase("create")) {
                     player.openInventory(IslandDifficulty.openInventoryDifficulty());
                     return true;
                 }
 
                 // DELETE
-                if (sub.equalsIgnoreCase(argsCompletion[1])) {
+                if (sub.equalsIgnoreCase("delete")) {
 
                     return true;
                 }
 
                 // CONFIG
-                if (sub.equalsIgnoreCase(argsCompletion[2])) {
+                if (sub.equalsIgnoreCase("config")) {
 
+                    return true;
+                }
+
+                // LIST
+                if (sub.equalsIgnoreCase("list")) {
+                    final PlayerIslandRequest playerIslandRequest = new PlayerIslandRequest(skyBlock.getDatabase(), false);
+
+                    try {
+                        if(!playerIslandRequest.playerHasIsland(player)) {
+                            player.sendMessage("§cVous n'avez pas d'île, créer vous en une avec la commande §6/island create");
+                            return true;
+                        }
+                        final List<IslandMate> islandMates = playerIslandRequest.islandMates(player);
+
+                        skyBlock.getDatabase().closeConnection();
+
+                        final IslandMate owner = islandMates.stream().parallel().filter(islandMate ->
+                                islandMate.status() == MateStatus.OWNER).findFirst().get();
+
+                        final StringBuilder messageBuilder = new StringBuilder("List des joueurs de votre île\n").append(owner.getStatus())
+                                .append(" ").append(owner.player().getName());
+
+                        islandMates.remove(owner);
+
+                        islandMates.stream().sorted(Comparator.comparingInt(IslandMate::isOnline)).forEach(islandMate ->
+                                messageBuilder.append("\n").append(islandMate.getStatus()).append(" ").append(islandMate.player().getName()));
+
+                        player.sendMessage(messageBuilder.toString());
+                    } catch(SQLException e) {
+                        throw new RuntimeException(e);
+                    }
                     return true;
                 }
                 return false;
@@ -49,19 +85,19 @@ public class IslandCommand implements CommandExecutor, TabCompleter {
 
             if (args.length == 2) {
                 // INVITE
-                if (sub.equalsIgnoreCase(argsCompletion[3])) {
+                if (sub.equalsIgnoreCase("invite")) {
 
                     return true;
                 }
 
                 // KICK
-                if (sub.equalsIgnoreCase(argsCompletion[4])) {
+                if (sub.equalsIgnoreCase("kick")) {
 
                     return true;
                 }
 
                 // SET OWNER
-                if (sub.equalsIgnoreCase(argsCompletion[5])) {
+                if (sub.equalsIgnoreCase("setowner")) {
 
                     return true;
                 }
