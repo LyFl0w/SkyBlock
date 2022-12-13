c4826f896434622afb0d628e5fca8708e6e1ceab
LyFlow <off.lyflow@gmail.com>
2022-12-14 00:48:46 +0100

    - patch world is created when the CreateIslandEvent is played

--- src/main/java/net/lyflow/skyblock/event/island/CreateIslandEvent.java
+++ src/main/java/net/lyflow/skyblock/event/island/CreateIslandEvent.java
@@ -3,11 +3,16 @@ package net.lyflow.skyblock.event.island;
 import net.lyflow.skyblock.SkyBlock;
 import net.lyflow.skyblock.island.IslandDifficulty;
 import net.lyflow.skyblock.island.PlayerIslandRequest;
+import net.lyflow.skyblock.utils.ResourceUtils;
+
+import org.bukkit.Location;
+import org.bukkit.WorldCreator;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import java.io.File;
 import java.sql.SQLException;
 
 public class CreateIslandEvent extends Event implements Cancellable {
@@ -21,16 +26,37 @@ public class CreateIslandEvent extends Event implements Cancellable {
             final PlayerIslandRequest playerIslandRequest = new PlayerIslandRequest(skyBlock.getDatabase(), false);
 
             if(playerIslandRequest.playerHasIsland(player)) {
-                player.sendMessage("Tu ne peux pas avoir plusieurs îles en même temps !");
+                player.sendMessage("§cTu ne peux pas avoir plusieurs îles en même temps !");
                 setCancelled(true);
                 return;
             }
+            player.sendMessage("§bCréation de votre île en cours §6§o(difficulté : "+islandDifficulty.name()+")");
 
+            // Set player has island in BDD
             playerIslandRequest.createIsland(player, islandDifficulty);
-
             skyBlock.getDatabase().closeConnection();
 
-            player.sendMessage("§bCréation de votre île en cours §6§o(difficulté : "+islandDifficulty.name()+")");
+            skyBlock.getServer().getScheduler().runTaskAsynchronously(skyBlock, () -> {
+                try {
+                    // Make a copy of  Island World
+                    final String defaultPath = "skyblock-map/"+playerIslandRequest.getPlayerID(player);
+                    final File islandWorld = new File(skyBlock.getDataFolder(), "../../"+defaultPath);
+                    ResourceUtils.saveResourceFolder("maps/skyblock-"+islandDifficulty.name().toLowerCase(), islandWorld, skyBlock, false);
+
+                    // Load World
+                    skyBlock.getServer().createWorld(new WorldCreator(defaultPath));
+
+                    player.sendMessage("§bFin de la création de votre île");
+
+                    // Teleport to the world
+                    player.teleport(new Location(skyBlock.getServer().getWorld(defaultPath), -0.5, 100, 0.5, 90, 0));
+
+                    player.sendMessage("§bTéléportation en cours");
+                } catch(SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
         } catch(SQLException e) {
             throw new RuntimeException("Erreur lors de la récupération de la base lors de la création d'une île", e);
         }
@@ -54,4 +80,5 @@ public class CreateIslandEvent extends Event implements Cancellable {
     public void setCancelled(boolean setCancelled) {
         this.isCancelled = setCancelled;
     }
+
 }
