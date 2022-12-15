17b2bcbbb8f81c2dd567a6fdfa33e5591353619e
LyFlow <off.lyflow@gmail.com>
2022-12-15 15:09:33 +0100

    - add register events
    - rewrite something

--- src/main/java/net/lyflow/skyblock/PluginManager.java
+++ src/main/java/net/lyflow/skyblock/PluginManager.java
@@ -2,8 +2,11 @@ package net.lyflow.skyblock;
 
 import net.lyflow.skyblock.command.IslandCommand;
 import net.lyflow.skyblock.listener.inventory.InventoryClickListener;
-import net.lyflow.skyblock.listener.player.PlayerJoinLister;
+import net.lyflow.skyblock.listener.player.AsyncPlayerPreLoginListener;
+import net.lyflow.skyblock.listener.player.PlayerJoinListener;
 
+import net.lyflow.skyblock.listener.player.PlayerQuitListener;
+import net.lyflow.skyblock.listener.player.PlayerToggleSneakListener;
 import org.bukkit.command.PluginCommand;
 
 public class PluginManager {
@@ -20,7 +23,10 @@ public class PluginManager {
     private void registerEvents(org.bukkit.plugin.PluginManager pluginManager) {
 
         // PLAYER EVENT
-        pluginManager.registerEvents(new PlayerJoinLister(skyblock), skyblock);
+        pluginManager.registerEvents(new PlayerQuitListener(skyblock), skyblock);
+        pluginManager.registerEvents(new PlayerJoinListener(skyblock), skyblock);
+        pluginManager.registerEvents(new AsyncPlayerPreLoginListener(skyblock), skyblock);
+        pluginManager.registerEvents(new PlayerToggleSneakListener(), skyblock);
 
         // INVENTORY EVENT
         pluginManager.registerEvents(new InventoryClickListener(skyblock), skyblock);
