04d0d01d46b352fc4c421c1105b0c8218e23f539
LyFlow <off.lyflow@gmail.com>
2022-12-17 13:12:25 +0100

    - rework on chance to get ore with cobblestone generator

--- src/main/java/net/lyflow/skyblock/listener/block/BlockSpreadListener.java
+++ src/main/java/net/lyflow/skyblock/listener/block/BlockSpreadListener.java
@@ -13,11 +13,11 @@ public class BlockSpreadListener implements Listener {
     public void onBlockSpread(BlockFormEvent event) {
         final Material dmaterial = event.getNewState().getType();
 
-        if(dmaterial == Material.COBBLESTONE && new SecureRandom().nextInt(10)+1 == 5) {
+        if(dmaterial == Material.COBBLESTONE && new SecureRandom().nextInt(20)+1 == 10) {
             final int rdm = new SecureRandom().nextInt(33)+1;
             Material material;
-            if(rdm <= 15) material = Material.COPPER_ORE;
-            else if(rdm <= 25) material = Material.IRON_ORE;
+            if(rdm <= 15) material = Material.IRON_ORE;
+            else if(rdm <= 25) material = Material.COPPER_ORE;
             else if(rdm <= 30) material = Material.GOLD_ORE;
             else if(rdm <= 32) material = Material.DIAMOND_ORE;
             else material = Material.EMERALD_ORE;
