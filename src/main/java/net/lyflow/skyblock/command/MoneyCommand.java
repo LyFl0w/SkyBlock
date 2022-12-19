e0d2a7e8c312c15a9d42e24944a2e20f3625080f
LyFlow <off.lyflow@gmail.com>
2022-12-19 23:17:08 +0100

    - patch bug for "/money" command

--- src/main/java/net/lyflow/skyblock/command/MoneyCommand.java
+++ src/main/java/net/lyflow/skyblock/command/MoneyCommand.java
@@ -33,22 +33,37 @@ public class MoneyCommand implements CommandExecutor{
             }
             if(args.length == 3) {
                 if(args[0].equalsIgnoreCase("send")){
-                    final AccountRequest accountRequest = new AccountRequest(skyBlock.getDatabase(), false);
-                    if(args[2].equals(player.getName())) {
+                    if(args[1].equals(player.getName())) {
                         player.sendMessage("§cVous ne pouvez pas vous envoyer de l'argent");
                         return true;
                     }
                     try {
-                        final float amount = Float.parseFloat(new DecimalFormat("0.00").format(Float.parseFloat(args[2])));
+                        final float amount = Math.round(Float.parseFloat(args[2]) * 100.0) / 100.0f;
+                        if(amount <= 0) {
+                            player.sendMessage("§cVous ne pouvez pas envoyer moins de 0$");
+                            return true;
+                        }
+
+                        final AccountRequest accountRequest = new AccountRequest(skyBlock.getDatabase(), false);
+                        final float playerMoney = accountRequest.getMoney(player.getUniqueId());
+                        if(playerMoney < amount) {
+                            skyBlock.getDatabase().closeConnection();
+                            player.sendMessage("§cVous n'avez pas "+amount+"$");
+                            return true;
+                        }
+
                         final OfflinePlayer offlinePlayer = accountRequest.getOfflinePlayerByName(args[1]);
                         if(offlinePlayer == null) {
+                            skyBlock.getDatabase().closeConnection();
                             player.sendMessage("§cLe joueur "+args[1]+" n'existe pas");
                             return true;
                         }
-                        accountRequest.setMoney(player.getUniqueId(), accountRequest.getMoney(player.getUniqueId()) - amount);
+
+                        accountRequest.setMoney(player.getUniqueId(),  - amount);
                         accountRequest.setMoney(offlinePlayer.getUniqueId(), accountRequest.getMoney(offlinePlayer.getUniqueId()) + amount);
                         if(offlinePlayer.isOnline()) offlinePlayer.getPlayer().sendMessage("§aVous avez reçu "+amount+" $ de "+player.getName());
                         player.sendMessage("§aVous avez envoyé "+amount+"$ à "+offlinePlayer.getName());
+                        skyBlock.getDatabase().closeConnection();
                     } catch(SQLException e) {
                         throw new RuntimeException(e);
                     } catch(NumberFormatException e){
